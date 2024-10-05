e93cdc6179d95c1b0ea2a6ed40e1abbb59320665
GugaAAndrade <gugucp2003@gmail.com>
2024-10-05 19:37:07 -0300

    add: MyLands Page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,4 +1,11 @@
 import { DatePickerDemo } from '@/components/dayPicker'
+import {
+  ThermometerSun,
+  CloudDrizzle,
+  MapPin,
+  Calendar,
+  Wind,
+} from 'lucide-react'
 
 export default function Dashboard() {
   return (
@@ -8,18 +15,54 @@ export default function Dashboard() {
           <h1 className="text-3xl"> Land 1</h1>
           <DatePickerDemo />
         </div>
-        <div className=" flex w-full h-[700px] bg-blue-100 gap-6">
-          <div className="w-1/4 h-full bg-red-200"></div>
-          <div className="flex-1 flex-col flex gap-6 bg-green-200">
-            <div className="flex h-1/3 w-full gap-6 bg-pink-200">
-              <div className="h-full w-1/3  bg-orange-200"></div>
-              <div className="h-full w-2/3 bg-orange-200"></div>
+        <div className=" flex w-full h-[700px]  gap-6">
+          <div className="w-1/4 h-full bg-white shadow-lg rounded-xl"></div>
+          <div className="flex-1 flex-col flex gap-6 ">
+            <div className="flex h-1/3 w-full gap-6 ">
+              <div className="h-full w-1/4 flex-col bg-white shadow-lg rounded-xl p-4 gap-3 flex ">
+                <div className=" w-full flex items-center gap-4">
+                  <ThermometerSun className="size-14" />{' '}
+                  <div className="text-4xl font-bold">28°</div>
+                </div>
+                <div className="flex w-full  items-center gap-2">
+                  <CloudDrizzle className="size-6" />{' '}
+                  <div className="text-lg">Rainy clouds</div>
+                </div>
+                <div className="h-px w-full bg-black" />
+                <div className="flex items-start gap-2">
+                  <MapPin className="size-6" />{' '}
+                  <div className="text-lg">Florida, MI</div>
+                </div>
+                <div className="flex gap-2">
+                  <Calendar className="size-6" />{' '}
+                  <div className="text-lg">5 Octorber 2024</div>
+                </div>
+              </div>
+              <div className="h-full w-3/4 bg-white shadow-lg rounded-xl"></div>
             </div>
-            <div className="flex h-1/3 w-full gap-6 bg-pink-200">
-              <div className="h-full w-1/2  bg-orange-200"></div>
-              <div className="h-full w-1/2 bg-orange-200"></div>
+            <div className="flex h-1/3 w-full gap-6 ">
+              <div className="h-full w-3/4 bg-white shadow-lg rounded-xl"></div>
+              <div className="h-full w-1/4 flex-col bg-white shadow-lg rounded-xl p-4 gap-3 flex ">
+                <div className=" w-full flex items-center gap-4">
+                  <Wind className="size-14" />{' '}
+                  <div className="text-4xl font-bold">
+                    13
+                    <span className="text-lg">km/h</span>
+                  </div>
+                </div>
+                <div className="flex w-full  items-center gap-2">
+                  <div className="text-lg">Vento fresco</div>
+                </div>
+                <div className="h-px w-full bg-black" />
+                <div className=" w-full flex items-center gap-4">
+                  <div className="text-2xl font-bold">Luminosity</div>
+                </div>
+                <div className="flex w-full  items-center gap-2">
+                  <div className="text-lg">80 lumens</div>
+                </div>
+              </div>
             </div>
-            <div className="h-1/3 w-full bg-orange-200"></div>
+            <div className="h-1/3 w-full bg-white shadow-lg rounded-xl"></div>
           </div>
         </div>
       </div>
